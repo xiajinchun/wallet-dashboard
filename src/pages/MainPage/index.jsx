41c9c6f682a6472fea4f8834437db7a2e0b4b713
Jinchun Xia <xiajinchunc@gmail.com>
2023-12-09 18:16:01 +0800

    update zksync apge

---
+++ src/pages/MainPage/index.jsx
@@ -0,0 +1,47 @@
+import MenuHeader from "@pages/MenuHeader/index.jsx";
+import { Outlet } from "react-router-dom";
+import { Layout } from "antd";
+
+function MainPage() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#fff"
+      }}
+    >
+      <Layout>
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            width: "100%",
+            zIndex: 1
+          }}
+        >
+          <MenuHeader
+            style={{
+              backgroundColor: "#fff",
+              borderBottom: "1px solid #e8e8e8",
+              boxShadow: "0 2px 8px rgba(0, 0, 0, 0.06)",
+              zIndex: 3
+            }}
+          />
+        </div>
+        <div
+          style={{
+            minHeight: "85vh",
+            backgroundColor: "#fff",
+            borderRadius: "4px",
+            marginTop: "42px"
+          }}
+        >
+          <div>
+            <Outlet />
+          </div>
+        </div>
+      </Layout>
+    </div>
+  );
+}
+
+export default MainPage;
---
+++ src/pages/MenuHeader/index.jsx
@@ -0,0 +1,127 @@
+import {Menu} from 'antd';
+import {useEffect, useState} from 'react';
+import {useLocation, useNavigate} from "react-router-dom";
+import {GithubOutlined, SettingOutlined, TwitterOutlined} from "@ant-design/icons";
+import './index.css'
+import React from "react";
+import {useTranslation} from "react-i18next";
+import {Select} from 'antd';
+import initPage from "@utils/initPage.js";
+
+const {Option} = Select;
+
+function LanguageSwitcher() {
+    const {i18n} = useTranslation();
+
+    const changeLanguage = (language) => {
+        i18n.changeLanguage(language);
+        localStorage.setItem('i18nextLng', language);
+    };
+
+    return (
+        <Select defaultValue={i18n.language} style={{width: 120}} onChange={changeLanguage}>
+            <Option value="zh">中文</Option>
+            <Option value="en">English</Option>
+        </Select>
+    );
+}
+
+const MenuHeader = () => {
+    const items = [
+        {
+            label: 'zkSync',
+            key: 'zksync',
+        },
+        {
+            label: 'zkInfo',
+            key: 'zk_info',
+        },
+        // {
+        //     label: 'Stark',
+        //     key: 'stark',
+        // },
+        // {
+        //     label: 'StarkInfo',
+        //     key: 'stark_info',
+        // },
+        // {
+        //     label: "Linea",
+        //     key: 'linea',
+        // },
+        // {
+        //     label: 'LayerZero',
+        //     key: 'layer',
+        // },
+        // {
+        //     label: 'Coffee',
+        //     key: 'coffee',
+        // },
+        // {
+        //     label: (
+        //         <a href="https://twitter.com/beibeieth" target="_blank" rel="noopener noreferrer">
+        //             <TwitterOutlined/>
+        //         </a>
+        //     ),
+        //     key: 'twitter',
+        // },
+        // {
+        //     label: (
+        //         <a href="https://github.com/wxtsky/MyWalletScan" target="_blank" rel="noopener noreferrer">
+        //             <GithubOutlined/>
+        //         </a>
+        //     ),
+        //     key: 'github',
+        // },
+        {
+            label: <SettingOutlined/>,
+            key: 'setting'
+        },
+        {
+            label: <LanguageSwitcher/>,
+            key: 'languageSwitch',
+        }
+    ];
+    const navigate = useNavigate();
+    const location = useLocation();
+    const [current, setCurrent] = useState(location.pathname.replace('/', '') || 'zksync');
+
+    const onClick = (e) => {
+        if (e.key === 'languageSwitch' || e.key === 'ethPrice') {
+            return;
+        }
+
+        setCurrent(e.key);
+    };
+    useEffect(() => {
+        initPage();
+    }, []);
+    useEffect(() => {
+        if (location.pathname.replace('/', '') === 'twitter' || location.pathname.replace('/', '') === 'github') {
+            return;
+        }
+        setCurrent(location.pathname.replace('/', '') || 'zksync');
+    }, [location.pathname]);
+
+    useEffect(() => {
+        if (current === 'twitter' || current === 'github') {
+            return;
+        }
+        navigate(`/${current}`);
+    }, [current]);
+
+    return (
+        <Menu
+            onClick={onClick}
+            selectedKeys={[current]}
+            mode="horizontal"
+            style={{
+                display: 'flex',
+                justifyContent: 'center'
+            }}
+            className="custom-menu"
+            items={items}
+        />
+    );
+};
+
+export default MenuHeader;
